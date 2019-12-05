b200af31b646e9389e1d40d9d0ab4663bd80ee2a
reshma728 <58553017+reshma728@users.noreply.github.com>
2019-12-05 16:22:48 +0530

    Add files via upload

---
+++ p3.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class P3
+{
+ public static void main(String args[])
+{
+  int n=Integer.parseInt(args[0]);
+  int m=Integer.parseInt(args[1]);
+   System.out.println(n+m);
+}
+}
